a5c03a1e97d99e4b94517f9535f51f3fb2ad7199
Eric Hopper <hopper@omnifarious.org>
2025-05-18 18:51:31 -0700

    Fleshed out Tick and Ticker, made Ticker contain a thread.

--- src/test_timer.cpp
+++ src/test_timer.cpp
@@ -21,7 +21,17 @@ class TimerState {
 using ticker_ctx_t = ::CMD2::Context<TimerState>;
 
 // A Command that actually executes the tick on the TimerState.
-class TimerState::Tick : public ticker_ctx_t::Command {};
+class TimerState::Tick : public ticker_ctx_t::Command, ::std::enable_shared_from_this<Tick> {
+   public:
+      Tick() = default;
+      ~Tick() override = default;
+      void operator()(ticker_ctx_t &ctx) override
+      {
+         auto &state = ctx.get_state();
+         state.tick_count_++;
+         state.commandlog_.emplace_back(ctx.get_state().tick_count_, shared_from_this());
+      }
+};
 
 
 // Holds the context for a thread that sends Tick Commands to the ticker_ctx_t
@@ -29,15 +39,40 @@ class Ticker {
  public:
    // The context must live while the Ticker is alive.
    Ticker(::std::uint32_t microseconds, ticker_ctx_t &ctx)
-      : microseconds_(microseconds), ctx_(ctx) {
+      : microseconds_(microseconds),
+      ctx_(ctx), last_tick_(::std::chrono::steady_clock::now()),
+      thread_([this](::std::stop_token stop_token)
+   {
+         this->run(stop_token);
+   })
+   {
    }
-   ~Ticker() {}
+   ~Ticker()
+   {
+      thread_.request_stop();
+   };
 
-   void run()
-   {}
+   void run(::std::stop_token stop_token)
+   {
+      while (!stop_token.stop_requested()) {
+         auto now = ::std::chrono::steady_clock::now();
+         auto diff = ::std::chrono::duration_cast<::std::chrono::microseconds>(now - last_tick_);
+         if (diff < microseconds_) {
+            auto const remaining_time = microseconds_ - diff;
+            ::std::this_thread::sleep_for(remaining_time);
+         }
+         last_tick_ += microseconds_;
+         ctx_.queue_to_context().enqueue(::std::make_shared<TimerState::Tick>());
+      }
+   }
+   void stop()
+   {
+      thread_.request_stop();
+   }
 
  private:
-   ::std::uint32_t microseconds_;
+   ::std::chrono::microseconds microseconds_;
    ticker_ctx_t &ctx_;
    ::std::chrono::steady_clock::time_point last_tick_;
+   ::std::jthread thread_;
 };
