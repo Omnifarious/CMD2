8971f959c552b790864e3ae1f2c9d5ac77f658f1
Eric Hopper <hopper@omnifarious.org>
2025-06-08 17:40:54 -0700

    Actually make test_timer test use moved Ticker, and fix any bugs.

--- src/Context.hpp
+++ src/Context.hpp
@@ -25,30 +25,31 @@ class Context {
     friend class Command;
 
     class Ticker {
-      class Tick : public Command {
-        public:
-          explicit Tick(const tickfunc_t& tickfunc) : tickfunc_(tickfunc) {}
-          ~Tick() override = default;
-          void operator()(Context& context) override
-          {
-            tickfunc_(context);
-          }
-        private:
-          const tickfunc_t& tickfunc_;
-      };
+     public:
+       class Tick : public Command {
+         public:
+           explicit Tick(const tickfunc_t& tickfunc) : tickfunc_(tickfunc) {}
+           ~Tick() override = default;
+           void operator()(Context& context) override
+           {
+              tickfunc_(context);
+           }
+         private:
+           const tickfunc_t& tickfunc_;
+       };
 
-      Ticker(::std::uint32_t microseconds, Context& ctx, const tickfunc_t& tickfunc) :
-      microseconds_(microseconds),
-      ctx_(ctx),
-      last_tick_(::std::chrono::steady_clock::now()),
-      thread_([this](::std::stop_token stop_token) {
-        this->run(stop_token);
-      }),
-      tickfunc_(tickfunc)
+       Ticker(::std::uint32_t microseconds, Context& ctx, const tickfunc_t& tickfunc) :
+            microseconds_(microseconds),
+            ctx_(ctx),
+            last_tick_(::std::chrono::steady_clock::now()),
+            thread_([this](::std::stop_token stop_token) {
+              this->run(stop_token);
+            }),
+            tickfunc_(tickfunc)
       {}
       ~Ticker()
       {
-        thread_.request_stop();
+         thread_.request_stop();
       }
 
       void run(::std::stop_token stop_token)
@@ -92,6 +93,10 @@ class Context {
     Context() = default;
     explicit Context(State state) : state_(::std::move(state)) {}
     explicit Context(State state, ::std::uint32_t microseconds, tickfunc_t tickfunc) : state_(::std::move(state))
+    {
+      ticker_ = ::std::make_unique<Ticker>(microseconds, *this, tickfunc);
+    }
+    explicit Context(::std::uint32_t microseconds, tickfunc_t tickfunc)
     {
       ticker_ = ::std::make_unique<Ticker>(microseconds, *this, tickfunc);
     }
--- src/test_timer.cpp
+++ src/test_timer.cpp
@@ -7,6 +7,10 @@
 #include <iostream>
 
 int main(int, char **);
+class TimerState;
+using ticker_ctx_t = ::skythedragon::ACE::Context<TimerState>;
+
+static void tick(ticker_ctx_t& ctx);
 
 class TimerState {
  public:
@@ -15,29 +19,15 @@ class TimerState {
    class Tick;
    friend class Tick;
    friend int main(int, char **);
+   friend void tick(ticker_ctx_t& ctx);
 
  private:
    using cmd_ptr_t = ::std::shared_ptr<void>;
    tick_t tick_count_ = 0;
-   using log_entry_t = ::std::pair<time_point_t, cmd_ptr_t>;
-   ::std::vector<log_entry_t> commandlog_;
+   ::std::vector<time_point_t> commandlog_;
 };
 
 using ticker_ctx_t = ::skythedragon::ACE::Context<TimerState>;  // a -> b -> c _> a   d -> c
-
-// A Command that actually executes the tick on the TimerState.
-class TimerState::Tick : public ticker_ctx_t::Command, public ::std::enable_shared_from_this<Tick> {
-   public:
-      Tick() = default;
-      ~Tick() override = default;
-      void operator()(ticker_ctx_t &ctx) override
-      {
-         auto &state = ctx.get_state();
-         state.tick_count_++;
-         auto me = shared_from_this();
-         state.commandlog_.emplace_back(::std::chrono::steady_clock::now(), me);
-      }
-};
 
 class Stop : public ticker_ctx_t::Command {
    public:
@@ -55,72 +45,34 @@ class Stop : public ticker_ctx_t::Command {
       static ::std::atomic<int> num_instances_;
 };
 
+static void tick(ticker_ctx_t& ctx)
+{
+   auto &state = ctx.get_state();
+   state.tick_count_++;
+   state.commandlog_.push_back(std::chrono::steady_clock::now());
+}
+
 ::std::atomic<int> Stop::num_instances_ = 0;
-
-// Holds the context for a thread that sends Tick Commands to the ticker_ctx_t
-class Ticker {
- public:
-   // The context must live while the Ticker is alive.
-   Ticker(::std::uint32_t microseconds, ticker_ctx_t &ctx)
-      : microseconds_(microseconds),
-      ctx_(ctx), last_tick_(::std::chrono::steady_clock::now()),
-      thread_([this](::std::stop_token stop_token)
-   {
-         this->run(stop_token);
-   })
-   {
-   }
-   ~Ticker()
-   {
-      thread_.request_stop();
-   };
-
-   void run(::std::stop_token stop_token)
-   {
-      while (!stop_token.stop_requested()) {
-         auto now = ::std::chrono::steady_clock::now();
-         auto diff = ::std::chrono::duration_cast<::std::chrono::microseconds>(now - last_tick_);
-         if (diff < microseconds_) {
-            auto const remaining_time = microseconds_ - diff;
-            ::std::this_thread::sleep_for(remaining_time);
-         }
-         last_tick_ += microseconds_;
-         ctx_.queue_to_context().enqueue(::std::make_shared<TimerState::Tick>());
-      }
-   }
-   void stop()
-   {
-      thread_.request_stop();
-   }
-
- private:
-   ::std::chrono::microseconds microseconds_;
-   ticker_ctx_t &ctx_;
-   ::std::chrono::steady_clock::time_point last_tick_;
-   ::std::jthread thread_;
-};
 
 int main(int, char **)
 {
    using ::std::shared_ptr;
    using ::std::make_shared;
    using ::std::cout;
-   ticker_ctx_t test_ctx;
+   ticker_ctx_t test_ctx(100'000, tick);
 
    ::std::jthread context_thread{&ticker_ctx_t::run, &test_ctx};
 
-   Ticker ticker(100000, test_ctx);
+   test_ctx.start_ticker();
 
    ::std::this_thread::sleep_for(::std::chrono::seconds(10));
-
-   ticker.stop();
 
    test_ctx.queue_to_context().enqueue(::std::make_shared<Stop>());
    context_thread.join();
    ::std::cout << "Tick count: " << test_ctx.get_state().tick_count_ << '\n';
    for (auto const& entry : test_ctx.get_state().commandlog_) {
-      ::std::cout << "At: " << entry.first.time_since_epoch()
-                  << " Command: " << entry.second.get() << '\n';
+      ::std::cout << "At: " << entry.time_since_epoch() << '\n';
    }
+
    return 0;
 }
